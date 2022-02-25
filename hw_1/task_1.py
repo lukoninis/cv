96088d63f1da26104cb5f4e80726ad06f2ac9df9
Ivan Lukonin <lukoninis@yandex.ru>
2022-02-25 11:39:25 +0300

    task 1

---
+++ hw_1/task_1.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+def main():
+    matrix = np.matrix([
+        [0.5, 2.16506351, 0.4330127],
+        [-0.8660254, 1.25, 0.25],
+        [0., 0.5, 2.5]
+    ])
+
+    u, s, vh = np.linalg.svd(matrix)
+
+    print(matrix)
+
+    ort_matrix = u * vh
+    print(ort_matrix)
+
+    # it seems like rotete over OZ.
+    # angle:
+    print((180 / np.pi) * np.arcsin(ort_matrix[0, 0]))
+
+
+if __name__ == "__main__":
+    main()
